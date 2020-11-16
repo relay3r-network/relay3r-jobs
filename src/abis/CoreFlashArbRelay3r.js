92eab88b00484af06484133b12821c8a79ba7476
HOVOH <71901758+HOVOH@users.noreply.github.com>
2020-11-16 15:15:44 -0500

    Fix unknown identifier: upkeep

--- src/abis/CoreFlashArbRelay3r.js
+++ src/abis/CoreFlashArbRelay3r.js
@@ -1,7 +1,7 @@
 const config = {
     address: "0x706a2effb344b9dE26A1816ca278255db22BFFdb",
     abi: [
-        "function workBatch(uint[] memory profitable) public upkeep",
+        "function workBatch(uint[] memory profitable) public",
         "function profitableStrats ( ) public view returns (uint[] memory)",
     ],
 };
