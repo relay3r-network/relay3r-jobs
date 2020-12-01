662051515a279e8aeb5ec5d40d773643c8217281
akshaynexus <akshaycm@hotmail.com>
2020-12-01 09:18:03 +0300

    Fix import for new job

--- src/JobHandler.js
+++ src/JobHandler.js
@@ -4,7 +4,7 @@ const {CoreFlashArbRelayerJob} = require("./jobs/relayer/CoreFlashArbRelayerJob"
 const {UnitradeRelayerJob} = require("./jobs/relayer/UnitradeRelayerJob");
 const {UniswapV2SlidingOracleJob} = require("./jobs/relayer/UniswapV2SlidingOracleJob");
 const {GetBackETHRelayerJob} = require("./jobs/relayer/GetBackETHRelayerJob");
-const {BACFarmerRelayerJob} = require("./jobs/relayer/BACFarmerRelayerJob");
+const {BACFarmerRelayerJob} = require("./jobs/relayer/BACFarmerRelayerv2Job");
 
 //Keeper jobs
 const {HegicPoolKeeperJob} = require("./jobs/keeper/HegicPoolKeeperJob");
