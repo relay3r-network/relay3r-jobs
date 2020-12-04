aaa0737595e60113e0660d9dc79a33b037cb3f12
akshaynexus <akshaycm@hotmail.com>
2020-12-04 23:55:25 +0300

    Update oracle job address

--- src/contracts/relayer/RelayerV1Oracle.js
+++ src/contracts/relayer/RelayerV1Oracle.js
@@ -1,5 +1,5 @@
 const config = {
-    address: "0xB98f3F6EeB490545940Cc8BA6AD68B49e071B2a7",
+    address: "0xA54b8DFB9B14357BF9BF8209Cb4fCe74BFeC660F",
     abi: [
       "function work (  ) external",
       "function workable (  ) external view returns ( bool )",
