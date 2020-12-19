f4de552f594fdfdd97260234e58cd73eb8c0a50a
akshaynexus <akshaycm@hotmail.com>
2020-12-19 17:07:07 +0300

    Add gaslimit

--- src/jobs/relayer/SynlRebalancerJob.js
+++ src/jobs/relayer/SynlRebalancerJob.js
@@ -11,6 +11,12 @@ class SynlRebalancerJob extends Job {
       provider
     );
   }
+  async callWork(gas) {
+    return await this.contract.work({
+      gasPrice: gas,
+      gasLimit:5395645
+    });
+  }
 }
 
 exports.SynlRebalancerJob = SynlRebalancerJob;
