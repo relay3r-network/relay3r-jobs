c19efe6beaafe149528293427bf61b49d5335215
akshaynexus <akshaycm@hotmail.com>
2020-12-19 21:21:27 +0300

    Bump gaslimit for rebalancer job

--- src/jobs/relayer/SynlRebalancerJob.js
+++ src/jobs/relayer/SynlRebalancerJob.js
@@ -14,7 +14,7 @@ class SynlRebalancerJob extends Job {
   async callWork(gas) {
     return await this.contract.work({
       gasPrice: gas,
-      gasLimit:5395645
+      gasLimit:9000000
     });
   }
 }
