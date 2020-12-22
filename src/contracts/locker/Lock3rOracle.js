41f3e927bf21e07f57fcdfda663c6d32d5c39c01
Lock3rDev <lock3r@programmer.net>
2020-12-22 13:38:16 +0000

    Create Lock3rOracle.js

---
+++ src/contracts/locker/Lock3rOracle.js
@@ -0,0 +1,8 @@
+const config = {
+  address: "0xd2C204B9C8FD5577D3EF3106Af9E77f8100B0A59",
+  abi: [
+    "function work (  ) external",
+    "function workable (  ) external view returns ( bool )",
+  ],
+};
+module.exports = config;
