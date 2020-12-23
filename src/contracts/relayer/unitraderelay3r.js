bd6fa33c9fe0a69e47d5438883b589b0d26877ec
akshaynexus <akshaycm@hotmail.com>
2020-12-23 22:40:44 +0300

    Add new updated unitrade job

--- src/contracts/relayer/unitraderelay3r.js
+++ src/contracts/relayer/unitraderelay3r.js
@@ -1,5 +1,5 @@
 const config = {
-  address: "0x09176410207689bf8727751633376a3Bf7725791",
+  address: "0xee334356dF4F75a3902346c88a819cDE41F896E6",
   abi: [
     "function workBatch (uint[] memory orderList) external",
     "function workSolo (uint order) external",
--- src/jobs/relayer/UnitradeRelayerJob.js
+++ src/jobs/relayer/UnitradeRelayerJob.js
@@ -2,7 +2,7 @@ const { Job } = require("../Job");
 const ethers = require("ethers");
 
 const contract = require("../../contracts/relayer/unitraderelay3r.js");
-
+const DebugJob = true;
 class UnitradeRelayerJob extends Job {
   constructor(account, provider) {
     super(
@@ -12,17 +12,42 @@ class UnitradeRelayerJob extends Job {
     );
   }
 
-  async callWork(gas) {
+  async getExecutableFiltered() {
     this.orderList = await this.contract.getExecutableOrdersList();
-    this.log.info(`Executing orders ${this.orderList.toString()}`);
-    if (this.orderList.length > 1)
-      return await this.contract.workBatch(this.orderList, {
-        gasPrice: gas,
-      });
-    else
-      return await this.contract.workSolo(this.orderList[0], {
-        gasPrice: gas,
-      });
+    //Now iterate and remove any that may not pass
+    for(const order of this.orderList) {
+      try {
+        await this.contract.callStatic.workSolo(order)
+        if(DebugJob) this.log.info(`${order} is executable`)
+        //If it worked,we have a executable one,do nothing
+      }catch(error){
+        if(DebugJob) this.log.info(`${order} is not executable`)
+        //This means the order fails at executing,so remove it from array
+        this.orderList = this.orderList.filter(item => item !== order)
+      }
+    }
+  }
+
+  async isWorkable() {
+    //Filter executable orders
+    await this.getExecutableFiltered();
+    //Workable if orderlist that is executable has more than 0 orders
+    return this.orderList.length > 0;
+  }
+
+  async callWork(gas) {
+    await this.getExecutableFiltered();
+    if(this.orderList.length > 0 )  {
+      this.log.info(`Executing orders ${this.orderList.toString()}`);
+      if (this.orderList.length > 1)
+        return await this.contract.workBatch(this.orderList, {
+          gasPrice: gas,
+        });
+      else
+        return await this.contract.workSolo(this.orderList[0], {
+          gasPrice: gas,
+        });
+    }
   }
 }
 
